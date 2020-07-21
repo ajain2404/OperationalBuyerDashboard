a8093ca64e2c8da4ee9e22218242514be872466a
S0016153758 <saravanan.narayanan@evonik.com>
2020-07-21 13:40:59 +0000

    Rel 2020.3

--- serviceBinding.js
+++
@@ -1,5 +0,0 @@
-function initModel() {
-	var sUrl = "/sap/opu/odata/sap/ZC_ANALYTICALCARD_CDS/";
-	var oModel = new sap.ui.model.odata.ODataModel(sUrl, true);
-	sap.ui.getCore().setModel(oModel);
-}
